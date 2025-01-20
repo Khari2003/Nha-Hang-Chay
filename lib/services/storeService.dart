b5fb748fa09c7afb9f4eb887adedbde904d9fda4
Khari2003 <99896250+Khari2003@users.noreply.github.com>
2025-01-20 13:46:29 +0700

    sửa lỗi api

--- lib/services/storeService.dart
+++ lib/services/storeService.dart
@@ -7,7 +7,7 @@ import 'package:http/http.dart' as http;
 class StoreService {
 
   static Future<List<Map<String, dynamic>>> fetchStoresData() async {
-    final response = await http.get(Uri.parse('https://secure-mesa-52472-4283a7099f0c.herokuapp.com//store/getall'));
+    final response = await http.get(Uri.parse('https://secure-mesa-52472-4283a7099f0c.herokuapp.com/store/getall'));
     if (response.statusCode == 200) {
       return List<Map<String, dynamic>>.from(json.decode(response.body));
     } else {
